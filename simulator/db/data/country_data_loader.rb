8b27b9f682036e41c442fa94e87f1f5e5db20db0
Gregory White <ideation200@me.com>
2014-09-26 16:55:24 -0700

    Implements methods ease the loading of data from stores
    Deletes the dashes from the name of the country
    Adds a get population data to the DataLoader module
    to create a better interface with the anyone consuming it

--- simulator/db/data/country_data_loader.rb
+++ simulator/db/data/country_data_loader.rb
@@ -1,10 +1,20 @@
+require 'debugger'
 module CountryDataLoader
   require 'json'
   def self.help
     puts "CountryDataLoader::get_country_data(country_code) => given a country code will return a ruby hash with all data for that country"
   end
 
-  def self.get_country_data(country_code)
-    JSON.parse( IO.read("countries_data/#{country_code}.json") )
+  def self.get_country_data(country_code,filepath)
+    JSON.parse( IO.read(filepath) )
+  end
+
+  def self.get_population_stats(country_code, filepath)
+    country_data_json = get_country_data(country_code,filepath)
+    population = country_data_json["people"]["population"]["text"].gsub(/ (.*est.)/,"")
+    growth = country_data_json["people"]["population_growth_rate"]["text"].gsub(/% (.*est.)/,"")
+    population = population.gsub(/,/,"").to_i
+    growth = growth.to_f
+    return population,growth
   end
 end
