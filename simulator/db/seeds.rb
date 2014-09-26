4b5e6ed5e4306344beb5863fce2cb313ad02cf23
Gregory White <ideation200@me.com>
2014-09-26 16:56:09 -0700

    Adds a state seeder to put all states into the db
    Grabs their name, code and population stats before
    creating an instance of that state

--- simulator/db/seeds.rb
+++ simulator/db/seeds.rb
@@ -1,1 +1,12 @@
+require_relative "data/countries.rb"
+require_relative "data/country_data_loader"
+
+countries_list = CountryList.get_country_list
+countries_list["all"].each do |country_code|
+  country = countries_list[country_code]
+  country = country.split(" ").map!{|word| word.capitalize!}.join(" ")
+  filepath = File.expand_path("../data/countries_data/#{country_code}.json", __FILE__)
+  population,growth = CountryDataLoader::get_population_stats(country_code, filepath)
+  State.create(name: country, country_code: country_code, population: population, population_growth: growth)
+end
 
