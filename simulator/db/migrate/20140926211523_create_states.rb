cc54a0c9e81a666aa6d7cafc932727fd5e50ec2f
Gregory White <ideation200@me.com>
2014-09-26 15:52:28 -0700

    Changes migrations, switch decimals to floats
    Accuracy of BigDecimals are not necessary and floats
    are easier to use in operations

--- simulator/db/migrate/20140926211523_create_states.rb
+++ simulator/db/migrate/20140926211523_create_states.rb
@@ -4,8 +4,8 @@ class CreateStates < ActiveRecord::Migration
       t.string :country_code
       t.string :name
       t.integer :population, default: 0
-      t.decimal :population_growth, default: 0.0
-      t.decimal :internet_penetration, default: 0.0
+      t.float :population_growth, default: 0.0
+      t.float :internet_penetration, default: 0.0
       t.timestamps
     end
   end
