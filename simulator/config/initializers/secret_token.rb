3c6a4c0dffcd16064ffce7ac5cb27938b825be08
Gregory White <ideation200@me.com>
2014-09-26 10:55:38 -0700

    Generates rails app that will store and manipulate country data

---
+++ simulator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simulator::Application.config.secret_token = '51d966342bae8971003a7d1439393abbced0a687e5a120842246612a56c44f03620dc7a945e7ec050d73d9009ffa1f4cdad23b24346f3de6cdccd52e9d877b84'
