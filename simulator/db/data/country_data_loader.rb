be77bfc6a54634ed865b8056a926df4b2fbfd2d7
Gregory White <ideation200@me.com>
2014-09-27 13:46:23 -0700

    Refactors & implements seeding the internet users data
    Encapsulates the seed methods into individual methods
    Moves the generation of the file path behind the scenes
    of the data loader interface.
    Uses a guard clause to cut down conditional lines 
    of code

--- simulator/db/data/country_data_loader.rb
+++ simulator/db/data/country_data_loader.rb
@@ -1,4 +1,4 @@
-require 'debugger'
+require 'csv'
 module CountryDataLoader
   require 'json'
   def self.help
@@ -9,12 +9,20 @@ module CountryDataLoader
     JSON.parse( IO.read(filepath) )
   end
 
-  def self.get_population_stats(country_code, filepath)
+  def self.get_population_stats(country_code)
+    filepath = File.expand_path("../countries_data/#{country_code}.json", __FILE__)
     country_data_json = get_country_data(country_code,filepath)
     population = country_data_json["people"]["population"]["text"].gsub(/ (.*est.)/,"")
     growth = country_data_json["people"]["population_growth_rate"]["text"].gsub(/% (.*est.)/,"")
     population = population.gsub(/,/,"").to_i
-    growth = growth.to_f
-    return population,growth
+    return population,growth.to_f
+  end
+
+  def self.get_internet_users(internet_users={})
+    filepath = File.expand_path("../raw_datasets/internet_users.csv", __FILE__)
+    CSV.foreach(filepath) do |row|
+      internet_users[row[0]] = (row[2].to_f)/100
+    end
+    return internet_users
   end
 end
--- simulator/db/seeds.rb
+++ simulator/db/seeds.rb
@@ -2,11 +2,23 @@ require_relative "data/countries.rb"
 require_relative "data/country_data_loader"
 
 countries_list = CountryList.get_country_list
-countries_list["all"].each do |country_code|
-  country = countries_list[country_code]
-  country = country.split(" ").map!{|word| word.capitalize!}.join(" ")
-  filepath = File.expand_path("../data/countries_data/#{country_code}.json", __FILE__)
-  population,growth = CountryDataLoader::get_population_stats(country_code, filepath)
-  State.create(name: country, country_code: country_code, population: population, population_growth: growth)
+def load_countries(countries_list)
+  countries_list["all"].each do |country_code|
+    country = countries_list[country_code]
+    country = country.split(" ").map!{|word| word.capitalize!}.join(" ")
+    population,growth = CountryDataLoader::get_population_stats(country_code)
+    State.create(name: country, country_code: country_code, population: population, population_growth: growth)
+  end
 end
 
+def load_internet_users(countries_list)
+  internet_users = CountryDataLoader::get_internet_users
+  internet_users.each_pair do |state_name, internet_penetration|
+    current_state = State.find_by_name(state_name)
+    next unless current_state
+    current_state.update_attributes(internet_penetration: internet_penetration)
+  end
+end
+
+load_countries(countries_list)
+load_internet_users(countries_list)
