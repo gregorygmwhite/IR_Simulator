394bbd7dcdb1e8d51c186b8d15d5af270bacc4c9
Gregory White <ideation200@me.com>
2014-09-30 17:08:01 -0700

    Implements calculator for relative economic power

--- simulator/app/models/power.rb
+++ simulator/app/models/power.rb
@@ -4,6 +4,7 @@ class Power < ActiveRecord::Base
 
   def calculate_raw_power
     calculate_projected_population
+    calculate_projected_economy
   end
 
   def calculate_projected_population
@@ -13,17 +14,37 @@ class Power < ActiveRecord::Base
     self.update_attributes!(raw_population_score: projected_population)
   end
 
-  def self.calculate_power
-    Power.calculate_population_power
+  def calculate_projected_economy
+    current_state = self.state
+    # 1/3 of projected growth in five years is the modifier
+    economy = current_state.economy.gdp_ppp
+    projected_growth = economy*((1+(current_state.economy.gdp_growth))**5) - economy
+    projected_economy = economy + projected_growth * 0.333333333333
+    self.update_attributes!(raw_economic_score: projected_economy)
   end
 
-  def self.calculate_population_power
+  def self.calculate_power
     top_population_score = (Power.order(:raw_population_score).last.raw_population_score).to_f
+    top_economic_score = (Power.order(:raw_economic_score).last.raw_economic_score).to_f
     State.all.each do |current_state|
-      # scales raw population score to a max score of 500
-      percentage = current_state.power.raw_population_score / top_population_score
-      population_score = 500 * percentage
-      current_state.update_attributes(:population_score => population_score)
+      current_state.power.calculate_population_power(top_population_score)
+      current_state.power.calculate_economic_power(top_economic_score)
     end
+  end
+
+  def calculate_population_power(top_population_score)
+    # scales raw population score to a max score of 500
+    current_state = self.state
+    percentage = self.raw_population_score / top_population_score
+    population_score = 500 * percentage
+    current_state.update_attributes(:population_score => population_score)
+  end
+
+  def calculate_economic_power(top_economic_score)
+    # scales raw economic score to a max score of 1000
+    current_state = self.state
+    percentage = self.raw_economic_score / top_economic_score
+    economic_score = 1000 * percentage
+    current_state.economy.update_attributes(:economic_score => economic_score)
   end
 end
