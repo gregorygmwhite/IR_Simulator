a2025a6f705882e166f7835eb32a145cb36681d1
Gregory White <ideation200@me.com>
2014-10-01 12:27:48 -0700

    Adds soft power point system to schema

--- simulator/app/models/goodness_index.rb
+++ simulator/app/models/goodness_index.rb
@@ -1,4 +1,11 @@
 class GoodnessIndex < ActiveRecord::Base
-  attr_accessible :state_id, :overall_rank, :science_rank, :cultural_rank, :peace_security_rank, :world_order_rank, :planet_climate_rank, :prosperity_equality_rank, :health_rank
+  attr_accessible :state_id, :overall_rank, :science_rank, :cultural_rank, :peace_security_rank, :world_order_rank, :planet_climate_rank, :prosperity_equality_rank, :health_rank, :points
   belongs_to :state
+
+  def self.set_goodness_points
+    count = GoodnessIndex.count + 1
+    GoodnessIndex.all.each do |country_index|
+      country_index.update_attributes!(points: (count- country_index.overall_rank))
+    end
+  end
 end
--- simulator/app/models/mnc.rb
+++ simulator/app/models/mnc.rb
@@ -1,4 +1,15 @@
 class Mnc < ActiveRecord::Base
-  attr_accessible :rank, :state_id, :name
+  attr_accessible :rank, :state_id, :name, :points
   belongs_to :state
+
+  def self.set_mnc_points
+    # grouped = Mnc.all.group_by{|mnc| mnc.state.name}
+    mnc_count = Mnc.count + 1
+    Mnc.all.each do |mnc|
+      current_state = mnc.state
+      points = (mnc_count - mnc.rank)
+      mnc.update_attributes(points: points)
+      current_state.update_attributes(mnc_points: (current_state.mnc_points + points))
+    end
+  end
 end
