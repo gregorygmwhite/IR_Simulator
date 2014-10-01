476e9b168ce18a3a53226715444fc56705f6e258
Gregory White <ideation200@me.com>
2014-10-01 12:30:12 -0700

    Adds default values of 0 to point system

--- simulator/db/migrate/20140930223507_add_population_score_to_states.rb
+++ simulator/db/migrate/20140930223507_add_population_score_to_states.rb
@@ -1,5 +1,5 @@
 class AddPopulationScoreToStates < ActiveRecord::Migration
   def change
-    add_column :states, :population_score, :integer
+    add_column :states, :population_score, :integer, default: 0
   end
 end
--- simulator/db/migrate/20140930234520_add_economic_score.rb
+++ simulator/db/migrate/20140930234520_add_economic_score.rb
@@ -1,5 +1,5 @@
 class AddEconomicScore < ActiveRecord::Migration
   def change
-    add_column :economies, :economic_score, :integer
+    add_column :economies, :economic_score, :integer, default: 0
   end
 end
