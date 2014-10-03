3235f63fca498e4f7e680ae5a3b57fd8b2f6f6a2
Gregory White <ideation200@me.com>
2014-10-03 14:52:44 -0700

    Test coverage for mmc model

--- simulator/spec/models/mnc_spec.rb
+++ simulator/spec/models/mnc_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Mnc, :type => :model do
+  let!(:mnc) {FactoryGirl.create(:mnc)}
+  it "should translate mnc rankings into point values" do
+    Mnc.set_mnc_points
+    expect(mnc.reload.points).to eq(1)
+  end
 end
