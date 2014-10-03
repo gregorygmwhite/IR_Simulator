7386b64d5e537b83e0dee16ad9d77e7ac2eeb0a1
Gregory White <ideation200@me.com>
2014-10-03 14:52:28 -0700

    Test coverage for military models

--- simulator/spec/models/navy_spec.rb
+++ simulator/spec/models/navy_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Navy, :type => :model do
+  let!(:us) {FactoryGirl.create(:state)}
+  it "should calculate the point value of the navy" do 
+    us.navy.calculate_points
+    expect(us.navy.points).to eq(16111000)
+  end
 end
