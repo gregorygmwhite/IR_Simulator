0a787aff1d612b68d2eb9767284d179dfad99219
Gregory White <ideation200@me.com>
2014-10-08 16:47:29 -0700

    Edits serializer to better format state info for angular use

--- simulator/app/models/state_serializer.rb
+++ simulator/app/models/state_serializer.rb
@@ -1,14 +1,16 @@
 class StateSerializer < ActiveRecord::Base
   def self.all_states
     all_states = State.order(:total_power_score).reverse
-    all_states.each_with_object(Hash.new(0)) do |state, memo|
-      memo[state.name] = {
+    all_states.each_with_object(Array.new) do |state, memo|
+      next if state.total_power_score < 1
+      state_info = {name: state.name,
         total_power_score: state.total_power_score,
+        military_score: state.military_score,
         economic_score: state.economic_score,
         soft_power_score: state.soft_power_score,
-        military_score: state.military_score,
         population_score: state.population_score
       }
+      memo.push( state_info )
     end
   end
 end
