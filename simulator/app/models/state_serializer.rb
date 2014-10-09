c87532d7a2827aee03ac6925e669761c0b22a417
Gregory White <ideation200@me.com>
2014-10-09 16:27:41 -0700

    Ajaxify the downloading of state information

--- simulator/app/models/state_serializer.rb
+++ simulator/app/models/state_serializer.rb
@@ -2,7 +2,7 @@ class StateSerializer < ActiveRecord::Base
   def self.all_states
     all_states = State.order(:total_power_score).reverse
     all_states.each_with_object(Array.new) do |state, memo|
-      next if state.total_power_score < 1
+      next if state.total_power_score < 2
       state_info = {name: state.name,
         total_power_score: state.total_power_score,
         military_score: state.military_score,
--- simulator/config/routes.rb
+++ simulator/config/routes.rb
@@ -1,4 +1,5 @@
 Simulator::Application.routes.draw do
   root :to => 'powerPresenter#welcome'
   match '/powers' => 'powerPresenter#power_index'
+  match '/states/index' => 'powerPresenter#get_states'
 end
