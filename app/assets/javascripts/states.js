a8846ef244800938bc7e2fa01b28b66bf90654c9
Gregory White <ideation200@me.com>
2014-10-09 17:23:45 -0700

    Changing setting to help with heroku deployment

--- app/assets/javascripts/states.js
+++ app/assets/javascripts/states.js
@@ -1,13 +1,13 @@
-var powerApp = angular.module('powerApp', []);
+var powerApp = angular.module('powerApp', [])
 
 powerApp.controller('powerDisplay', function ($scope) {
   $scope.getStates = function(){
-    $.ajax({
+    var request = $.ajax({
       url: "states/index",
       type: "GET",
       dataType: "json"
     })
-    .done(function( data ) {
+    request.done(function( data ) {
       $scope.states = data
       $scope.$apply()
     })
