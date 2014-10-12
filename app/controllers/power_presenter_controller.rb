744d1875f263dfff3f9dbab33de6a1ed56980cf8
Gregory White <ideation200@me.com>
2014-10-11 18:41:36 -0700

    Creates route&controller support for methodologies pages

--- app/controllers/power_presenter_controller.rb
+++ app/controllers/power_presenter_controller.rb
@@ -11,4 +11,9 @@ class PowerPresenterController < ApplicationController
   def get_states
     render json: StateSerializer.all_states
   end
+
+  def method_pages
+    page_name = params[:power_type]
+    render page_name.to_sym
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Simulator::Application.routes.draw do
   root :to => 'powerPresenter#welcome'
   match '/powers' => 'powerPresenter#power_index'
   match '/states/index' => 'powerPresenter#get_states'
+  match '/methods/:power_type' => 'powerPresenter#method_pages'
 end
