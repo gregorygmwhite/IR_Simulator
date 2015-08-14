4dd510d010df8a6c7dc41ec51f8a20139b2d9f28
Greg White <gwhite@dayspring-tech.com>
2015-08-14 16:41:01 -0700

    fix deploy scripts to allow from scratch opsworks deploy

--- deploy/before_migrate.rb
+++
@@ -1,1 +0,0 @@
-run 'cd /srv/www/ir_simulator/current/ && /usr/local/bin/bundle exec rake db:drop db:create'
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,1 +1,1 @@
-run 'cd /srv/www/ir_simulator/current/ && /usr/local/bin/bundle exec rake db:seed'
+run 'cd /srv/www/ir_simulator/current/ && /usr/local/bin/bundle exec rake db:drop db:create db:migrate db:seed'
